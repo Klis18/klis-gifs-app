3c9772c1d7c33dae7ef5c1dadfe7ef2ca4b55ae9
Klis18 <klisnp18@gmail.com>
2025-03-03 15:09:20 -0500

    creación de estuctura de componentes

---
+++ src/app/gifs/components/gif-list/gif-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gif-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './gif-list.component.html',
+  styles: ``
+})
+export class GifListComponent {
+
+}
