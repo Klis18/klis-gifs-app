aa71c9fbb3ec7e1fe4c7ea868380d27b42dde661
Klis18 <klisnp18@gmail.com>
2025-03-03 15:04:05 -0500

    creando páginas y configurando rutas iniciales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path:'dashboard',
+        loadComponent:() => import('./gifs/pages/dashboard/dashboard.component'),
+        children:[
+            {
+                path:'tendencias',
+                loadComponent: () => import('./gifs/pages/trending/trending.component')
+            },
+            {
+                path:'buscador',
+                loadComponent: () => import('./gifs/pages/trending/trending.component')
+            },
+            {
+                path:'**',
+                redirectTo: 'tendencias'
+            }
+        ]
+    },
+    {
+        path:'**',
+        redirectTo:'dashboard'
+    }
+];
---
+++ src/app/gifs/pages/trending/trending.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trending',
+  standalone: true,
+  imports: [],
+  templateUrl: './trending.component.html',
+  styles: ``
+})
+export default class TrendingComponent {
+
+}
